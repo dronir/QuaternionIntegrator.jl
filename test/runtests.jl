76d365b939e12a863348beef2fc51eb3ff9fd3ff
Olli Wilkman <olli.wilkman@iki.fi>
2019-09-18 16:14:41 +0200

    Fix tests after interface changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,13 +14,22 @@ using LinearAlgebra
     @test rotate(q, [0.0, 0.0, 1.0]) ≈ [0.0, 0.0, 1.0]
 end
 
+@testset "Orientation function" begin
+    v = [0.0, 0.0, 1.0]
+    
+    @test orientation(v, 0.0) ≈ Quaternion(1.0, 0.0, 0.0, 0.0)
+    @test orientation(v, π/2) ≈ Quaternion(cos(π/4), 0.0, 0.0, sin(π/4))
+    @test orientation(v, π) ≈ Quaternion(cos(π/2), 0.0, 0.0, sin(π/2))
+    
+end
+
 
 @testset "Step" begin
     # Set up test
     
     ∆t = 0.001
     I = diagm([1.0, 1.0, 1.0])
-    torque = QuaternionIntegrator.dummy_torque
+    torque(q) = [0.1, 0.0, 0.0]
     q0 = Quaternion(1.0, 0.0, 0.0, 0.0)
     w0 = [0.0, 0.0, 1.0]
     
@@ -34,15 +43,15 @@ end
     # Set up tests
     ∆t = 0.001
     I = diagm([1.0, 1.0, 1.0])
-    torque = q -> [0.1, 0.0, 0.0]
+    torque(q) = [0.1, 0.0, 0.0]
     q0 = Quaternion(1.0, 0.0, 0.0, 0.0)
     w0 = [0.0, 0.0, 0.0]
     N = 1
     
     q1, w1 = integrate(q0, w0, I, ∆t, torque)
-    Q, W = integrate(q0, w0, I, ∆t, torque, N)
+    qn, wn = integrate(q0, w0, I, ∆t, torque, N)
 
-    @test Q[2] ≈ q1
-    @test W[2] ≈ w1
+    @test qn ≈ q1
+    @test wn ≈ w1
 
 end
